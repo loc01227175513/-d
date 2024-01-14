4fe2900e6ec21504efa84fb6f87093bca7d37927
loc01227175513 <51218059+loc01227175513@users.noreply.github.com>
2024-01-14 23:13:10 +0700

    Create demo1.js

---
+++ loc/src/demo1.js
@@ -0,0 +1,15 @@
+import { StyleSheet,Text,View} from 'react-native';
+import React, { memo } from 'react';
+
+const demo1 = memo(() => {
+    return (
+        <View>
+            <text>demo1</text>
+            <text>loc </text>
+        </View>
+    );
+});
+
+export default demo1;
+const styles = StyleSheet.create({})
+//branch demo1
